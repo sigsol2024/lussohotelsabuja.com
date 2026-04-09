ca03c4e87c0cac11245ffe6d7aa8e41d8e422524
carter46 <mr.carter.tech07@gmail.com>
2026-04-09 04:34:38 +0100

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -5,8 +5,14 @@
     <div id="toastContainer" class="toast-container"></div>
     <?php require_once __DIR__ . '/media-modal.php'; ?>
 
-    <script src="<?= ADMIN_URL ?>assets/js/admin.js"></script>
-    <script src="<?= ADMIN_URL ?>assets/js/media-library.js"></script>
+    <?php
+      $adminJs = __DIR__ . '/../assets/js/admin.js';
+      $mediaJs = __DIR__ . '/../assets/js/media-library.js';
+      $adminV = is_file($adminJs) ? (string)filemtime($adminJs) : '1';
+      $mediaV = is_file($mediaJs) ? (string)filemtime($mediaJs) : '1';
+    ?>
+    <script src="<?= ADMIN_URL ?>assets/js/admin.js?v=<?= htmlspecialchars($adminV, ENT_QUOTES, 'UTF-8') ?>"></script>
+    <script src="<?= ADMIN_URL ?>assets/js/media-library.js?v=<?= htmlspecialchars($mediaV, ENT_QUOTES, 'UTF-8') ?>"></script>
 </body>
 </html>
 
